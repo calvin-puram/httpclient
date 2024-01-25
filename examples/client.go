3e27cb708ba6967ccd3d89cd325e0d201f3e7bf4
Calvin Job Puram <puram.calvin@gmail.com>
2024-01-25 14:02:24 +0100

    Update client.go

--- examples/client.go
+++ examples/client.go
@@ -14,7 +14,7 @@ var (
 func getClient() ghttp.Client {
 	commonHeader := make(http.Header)
 	commonHeader.Set("Content-Type", "application/json")
-	commonHeader.Set("Authorization", "Bearer "+ "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")
+	commonHeader.Set("Authorization", "Bearer "+ "xxx")
 
 	client := ghttp.NewBuilder().SetDisableTimeouts(false).SetMaxIdleConns(5).SetResponseHeaderTimeout(3*time.Second).SetHeaders(commonHeader).Build()
 	return client
